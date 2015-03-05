a6259bc2962a813fe1d9cdc6651952348156530c
rickjcr3 <rickrieta@gmail.com>
2015-03-04 23:16:57 -0500

    Update Partnership model to allow lookups for giver and getter

--- app/models/partnership.rb
+++ app/models/partnership.rb
@@ -1,6 +1,7 @@
 class Partnership < ActiveRecord::Base
   belongs_to :event
-  belongs_to :user
+  belongs_to :giver, class_name: 'User', foreign_key: 'giver_id'
+  belongs_to :getter, class_name: 'User', foreign_key: 'getter_id'
   has_one    :wishlist
 
   validate :cannot_get_self
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,6 @@ class User < ActiveRecord::Base
   has_many :roles
   has_many :events
   has_many :partnerships, foreign_key: 'giver_id'
-  has_many :partnerships, foreign_key: 'getter_id'
 
   has_many :invitations, class_name: 'Invite', foreign_key: 'sender_id'
   has_many :sent_invites, class_name: 'Invite', foreign_key: 'recipient_id'
