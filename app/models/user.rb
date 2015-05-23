37119172a3239fb5fa914cac7bbfb0c284d2ff12
rickjcr3 <rickrieta@gmail.com>
2015-05-23 13:32:30 -0400

    Add name columns to users and update the wishlist view to include a full name

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,8 @@ class User < ActiveRecord::Base
   has_one  :wishlist
   has_many :invitations, class_name: 'Invite', foreign_key: 'sender_id'
   has_many :sent_invites, class_name: 'Invite', foreign_key: 'recipient_id'
+
+  def full_name
+    "#{fname} #{lname}"
+  end
 end
