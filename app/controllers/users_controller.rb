7cd9e216d300e5ea5213b6d9b38603b8173461e9
rickjcr3 <rickrieta@gmail.com>
2015-06-04 21:57:03 -0400

    Add Raleway and change layout for events#show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,27 @@
 class UsersController < ApplicationController
-  before_action :get_event
+  before_action :get_event, only:[:index]
 
   def index
     @users = @event.users
   end
 
+  def edit
+    @user = current_user
+  end
+
+  def update
+    @user = current_user
+    @user.update_attributes(user_params)
+
+    if @user.save!
+      redirect_to events_path
+    end
+  end
+
+
   private
 
-  def get_event
-    @event = Event.find(params[:event_id])
+  def user_params
+    params.require(:user).permit(:fname, :lname, :email)
   end
 end
