b2cb48171c1c5b39b7cfd2a5db50165b86e0e285
rickjcr3 <rickrieta@gmail.com>
2015-09-09 23:33:02 -0400

    Config updates

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,15 @@ Swapthing::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+      address: "smtp.gmail.com",
+      port: 587,
+      domain: "gmail.com",
+      user_name: "mydevappmailer@gmail.com",
+      password: ENV['GMAIL_PW']
+  }
+  config.action_mailer.perform_deliveries = true
 end
