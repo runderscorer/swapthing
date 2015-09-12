1a35871e25efc1411e444da373fff885d5a3b206
rickjcr3 <rickrieta@gmail.com>
2015-09-12 12:43:07 -0400

    Add error handling to sign in

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -5,6 +5,17 @@ class Users::SessionsController < Devise::SessionsController
   end 
 
   def create
-		super
+    self.resource = warden.authenticate(auth_options)
+    if self.resource
+      set_flash_message(:notice, :signed_in) if is_flashing_format?
+      sign_in(resource_name, resource)
+      yield resource if block_given?
+      respond_with resource, location: after_sign_in_path_for(resource)
+    else
+      flash[:error] = 'Invalid email or password.'
+      redirect_to new_user_session_path
+    end
   end
+
 end
+
