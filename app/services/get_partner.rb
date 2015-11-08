b4ea8cfedac26dfe92025d06b128335f5560e5d6
rickjcr3 <rickrieta@gmail.com>
2015-11-08 12:15:09 -0500

    Fix service method logic for events with partnerships and users that are not yet assigned

--- app/services/get_partner.rb
+++ app/services/get_partner.rb
@@ -1,7 +1,12 @@
 class GetPartner
   def self.call(user, event_id)
-    if Event.find(event_id).partnerships.present?
-      Partnership.find_by(event_id: event_id, giver_id: user.id).getter
+    partnerships = Event.find(event_id).partnerships
+    return unless partnerships
+
+    if partnerships.include? giver_id: user.id
+      partner = partnerships.find_by(giver_id: user.id).getter
+    else
+      return nil
     end
   end
 end
