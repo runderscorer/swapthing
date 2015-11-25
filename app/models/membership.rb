83570e04a37d77fb14fbf36575b48844eba46893
rickjcr3 <rickrieta@gmail.com>
2015-11-25 10:37:40 -0500

    Add validation to prevent duplicate memberships in the scope of an event

--- app/models/membership.rb
+++ app/models/membership.rb
@@ -2,5 +2,5 @@ class Membership < ActiveRecord::Base
   belongs_to :user
   belongs_to :event
   delegate   :email, to: :user, prefix: true
-
+  validates_uniqueness_of :user_id, scope: :event_id, message: 'already exists'
 end
