b1a308c6e2f70a0e1d448eb851d451d1f4133bd4
rickjcr3 <rickrieta@gmail.com>
2015-11-24 20:25:49 -0500

    Fix broken service that was not properly displaying the partner

--- app/services/get_partner.rb
+++ app/services/get_partner.rb
@@ -3,7 +3,7 @@ class GetPartner
     partnerships = Event.find(event_id).partnerships
     return unless partnerships
 
-    if partnerships.include? giver_id: user.id
+    if partnerships.exists? giver_id: user.id
       partner = partnerships.find_by(giver_id: user.id).getter
     else
       return nil
