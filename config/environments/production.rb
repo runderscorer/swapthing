c3ec57693bead0d1a5724757d3c533f5521aa44d
reekjam <rickrieta@gmail.com>
2016-12-10 15:02:28 -0500

    Update mail config and ruby version

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,7 +90,7 @@ Swapthing::Application.configure do
   }
   config.action_mailer.perform_deliveries = true
 
-  config.action_mailer.default_url_options = { host: 'swap-thing.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'www.letsswapthings.com' }
 
   config.paperclip_defaults = {
     storage: :s3,
