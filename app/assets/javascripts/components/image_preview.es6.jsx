5217067bf91f26221dcecebc63b02df166b5ff3f
Rick Rieta <rick@vermonster.com>
2017-02-21 17:23:40 -0500

    Validate image src by file type

--- app/assets/javascripts/components/image_preview.es6.jsx
+++ app/assets/javascripts/components/image_preview.es6.jsx
@@ -1,14 +1,26 @@
 class ImagePreview extends React.Component {
+  validTypes() {
+    return ['.jpg', '.jpeg', '.gif', '.png'];
+  }
+
   displayImagePreview() {
     const { imageUrl } = this.props;
 
     if (imageUrl) {
-      return(
-        <div className='image_url_preview'>
-          <img src={imageUrl} style={{ maxHeight: '300px' }} />
-        </div>
-      );
+      if (this.validateUrlByFileType(imageUrl)) {
+        return(
+          <div className='image_url_preview'>
+            <img src={imageUrl} style={{ maxHeight: '300px' }} />
+          </div>
+        );
+      }
     }
+  }
+
+  validateUrlByFileType(src) {
+    let url = src;
+
+    return this.validTypes().includes(url.substr(url.lastIndexOf('.'), url.length))
   }
 
   render () {
