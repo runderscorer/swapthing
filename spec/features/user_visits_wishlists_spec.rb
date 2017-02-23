03e326c3050a9a38a0e37e9e1c4e3f5000118ebb
Rick Rieta <rick@vermonster.com>
2017-02-23 12:12:27 -0500

    Add edit item spec and update wishlists page object

--- spec/features/user_visits_wishlists_spec.rb
+++ spec/features/user_visits_wishlists_spec.rb
@@ -1,28 +1,48 @@
 require 'spec_helper'
 
 describe 'User visits wishlist page' do
-  before(:each) do
-    user = FactoryGirl.create(:user)
+  context 'adding an item' do
+    before(:each) do
+      user = FactoryGirl.create(:user)
 
-    sign_in(user.email, user.password)
+      sign_in(user.email, user.password)
+    end
+
+    it 'should open Add Item form' do
+      wishlist_page = Pages::Wishlists.new
+      wishlist_page.index_no_items
+      wishlist_page.open_form
+
+      expect(page).to have_content 'Add Item'
+    end
+
+    it 'should create item' do
+      wishlist_page = Pages::Wishlists.new
+      wishlist_page.index_no_items
+      wishlist_page.open_form
+      wishlist_page.create('Scary Stories', 'paperback', 15)
+
+      expect(page).to have_content 'Scary Stories'
+      expect(page).to have_content 'paperback'
+      expect(page).to have_content '$15'
+    end
   end
+  
+  context 'editing an item' do
+    before do
+      user = FactoryGirl.create(:user)
+      wishlist = FactoryGirl.create(:wishlist, user_id: user.id)
+      @item = FactoryGirl.create(:item, wishlist_id: wishlist.id)
 
-  it 'should open Add Item form' do
-    wishlist_page = Pages::Wishlists.new
-    wishlist_page.index
-    wishlist_page.open_form
+      sign_in(user.email, user.password)
+    end
 
-    expect(page).to have_content 'Add Item'
-  end
+    it 'should edit an item' do
+      wishlist_page = Pages::Wishlists.new
+      wishlist_page.index_with_items
+      wishlist_page.edit_name(@item.id, 'Twilight Zone')
 
-  it 'should create item' do
-    wishlist_page = Pages::Wishlists.new
-    wishlist_page.index
-    wishlist_page.open_form
-    wishlist_page.create('Scary Stories', 'paperback', 15)
-
-    expect(page).to have_content 'Scary Stories'
-    expect(page).to have_content 'paperback'
-    expect(page).to have_content '$15'
+      expect(page).to have_content 'Twilight Zone'
+    end
   end
 end
--- spec/helpers/pages/wishlists.rb
+++ spec/helpers/pages/wishlists.rb
@@ -2,9 +2,15 @@ module Pages
   class Wishlists
     include Capybara::DSL
 
-    def index
+    def index_no_items
       within('ul.navbar-list') do
         click_link 'Create Wishlist'
+      end
+    end
+
+    def index_with_items
+      within('ul.navbar-list') do
+        click_link 'My Wishlist'
       end
     end
 
@@ -21,6 +27,15 @@ module Pages
         fill_in 'Price', with: price
 
         click_button 'Add Item'
+      end
+    end
+
+    def edit_name(item_id, new_name)
+      find("#edit-item-#{item_id}").click
+      within('.form_container') do
+        fill_in 'Item Name', with: new_name
+
+        click_button 'Edit Item'
       end
     end
   end
