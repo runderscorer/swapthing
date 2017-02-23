7502637e175a80458b99260099f72c77ff86770a
Rick Rieta <rick@vermonster.com>
2017-02-23 13:46:49 -0500

    Add edit event feature spec

--- spec/features/user_creates_event_spec.rb
+++ spec/features/user_creates_event_spec.rb
@@ -1,32 +1,51 @@
 require 'spec_helper'
 
 describe 'User creating event' do
-  before(:each) do
-    user = FactoryGirl.create(:user)
+  context 'adding event' do
+    before(:each) do
+      user = FactoryGirl.create(:user)
 
-    sign_in(user.email, user.password)
+      sign_in(user.email, user.password)
+    end
+
+    it 'should display Add Event form' do
+      events_page = Pages::Events.new
+      events_page.open_form
+
+      expect(page).to have_content 'Add Event'
+    end
+
+    it 'should display errors' do
+      events_page = Pages::Events.new
+      events_page.open_form
+      events_page.submit_empty_form
+
+      expect(page).to have_content 'Required field'
+    end
+
+    it 'should create event' do
+      events_page = Pages::Events.new
+      events_page.open_form
+      events_page.create('Wrestlemania', 'Wrasslin time!', 500)
+
+      expect(page).to have_content 'Wrestlemania'
+    end
   end
 
-  it 'should display Add Event form' do
-    events_page = Pages::Events.new
-    events_page.open_form
+  context 'editing event' do
+    before(:each) do
+      user = FactoryGirl.create(:user)
+      @event = FactoryGirl.create(:event, admin_id: user.id)
+      FactoryGirl.create(:membership, event_id: @event.id, user_id: user.id)
 
-    expect(page).to have_content 'Add Event'
-  end
+      sign_in(user.email, user.password)
+    end
 
-  it 'should display errors' do
-    events_page = Pages::Events.new
-    events_page.open_form
-    events_page.submit_empty_form
+    it 'should update the event name' do
+      events_page = Pages::Events.new
+      events_page.edit_name(@event.id, 'Halloween Bash')
 
-    expect(page).to have_content 'Required field'
-  end
-
-  it 'should create event' do
-    events_page = Pages::Events.new
-    events_page.open_form
-    events_page.create('Wrestlemania', 'Wrasslin time!', 500)
-
-    expect(page).to have_content 'Wrestlemania'
+      expect(page).to have_content 'Halloween Bash'
+    end
   end
 end
