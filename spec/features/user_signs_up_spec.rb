e6e1e9345a89278e863b377069fc3deb8035f766
Rick Rieta <rick@vermonster.com>
2017-02-27 11:13:55 -0800

    Add new tests to user registration spec

--- spec/features/user_signs_up_spec.rb
+++ spec/features/user_signs_up_spec.rb
@@ -12,5 +12,25 @@ describe 'User signs up' do
 
     expect(page).to have_content 'Required field', count: 4
   end
+  
+  it 'should validate email address' do
+    registration_page = Pages::Registrations.new
+    registration_page.submit_with_invalid_email
 
+    expect(page).to have_content 'Invalid email address'
+  end
+
+  it 'should check that passwords match' do
+    registration_page = Pages::Registrations.new
+    registration_page.submit_with_non_matching_passwords
+
+    expect(page).to have_content 'Passwords do not match'
+  end
+
+  it 'should validate password' do
+    registration_page = Pages::Registrations.new
+    registration_page.submit_with_invalid_password
+
+    expect(page).to have_content 'Password must be at least 8 characters'
+  end
 end
--- spec/helpers/pages/registrations.rb
+++ spec/helpers/pages/registrations.rb
@@ -5,5 +5,32 @@ module Pages
     def submit_empty_form
       click_button 'Sign up'
     end
+
+    def submit_with_invalid_email
+      fill_in 'First Name', with: 'Ultimate'
+      fill_in 'Last Name', with: 'Warrior'
+      fill_in 'Email', with: 'email'
+      fill_in 'Password', with: 'password'
+      fill_in 'Confirm Password', with: 'password'
+      click_button 'Sign up'
+    end
+
+    def submit_with_non_matching_passwords
+      fill_in 'First Name', with: 'Ultimate'
+      fill_in 'Last Name', with: 'Warrior'
+      fill_in 'Email', with: 'email'
+      fill_in 'Password', with: 'password1'
+      fill_in 'Confirm Password', with: 'password2'
+      click_button 'Sign up'
+    end
+
+    def submit_with_invalid_password
+      fill_in 'First Name', with: 'Ultimate'
+      fill_in 'Last Name', with: 'Warrior'
+      fill_in 'Email', with: 'email'
+      fill_in 'Password', with: 'pw'
+      fill_in 'Confirm Password', with: 'pw'
+      click_button 'Sign up'
+    end
   end
 end
