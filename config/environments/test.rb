bcf82efdcbd02c23782bce178362584292058cb5
Rick Rieta <rick@vermonster.com>
2017-02-27 15:37:54 -0800

    Add missing host to test environment

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -31,6 +31,8 @@ Swapthing::Application.configure do
   # ActionMailer::Base.deliveries array.
   config.action_mailer.delivery_method = :test
 
+  config.action_mailer.default_url_options = { host: 'lvh.me:3000' }
+
   # Print deprecation notices to the stderr.
   config.active_support.deprecation = :stderr
 end
