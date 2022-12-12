ac93f2d2bd5c83c35ebc0661b8acbd8c1004dd12
Rick Rieta <rickrieta@gmail.com>
2022-12-11 18:24:22 -0800

    Use active storage to attach image files

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,12 +1,51 @@
 class Item < ActiveRecord::Base
   belongs_to :wishlist
-  has_one_attached :image
+  has_one_attached :image, service: :amazon
 
   validates_presence_of :name, :price, :wishlist_id
+  validate :image_url_format
+  validate :image_file_type
+
+  before_save :attach_image
 
   scope :ordered_by_name, -> { order(name: :asc) }
 
   def short_url
     self.url.split('.')[1]
+  end
+
+  private
+
+  def attach_image
+    return unless image_url
+
+    # Parse string and download image
+    parser = URI::Parser.new
+    url = parser.parse(image_url)
+    downloaded_image = url.open
+
+    filename = url.path.match(/(?!.*\/).+/).to_s 
+    # Negative lookahead  (?!
+    # Match zero or more of any character .*
+    # Match a forward slash \/
+    # End negative lookahead )
+    # Match one or more characters .+
+
+    # Create ActiveStorage records and upload to S3
+    image.attach(io: downloaded_image, filename: filename)
+  rescue
+    errors.add(:image_url, 'There was a problem attaching an image')
+  end
+
+  def image_url_format
+    if self.image_url.present? && !URI.parse(self.image_url).is_a?(URI::HTTP || URI::HTTPS)
+      errors.add(:image_url, 'Enter a valid image URL')
+    end
+  end
+
+  def image_file_type
+    if image.attached? && !image.content_type.in?(%w(image/jpeg image/png image/svg image/gif))
+      errors.add(:image_url, 'Enter a URL for a valid image file type')
+    end
   end
 end
