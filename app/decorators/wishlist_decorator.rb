035bca8835d90afdaf9d1e31b14481ac5d2bd94c
Rick Rieta <rickrieta@gmail.com>
2022-12-15 00:12:01 -0800

    Style updates and remove unused files

--- app/decorators/wishlist_decorator.rb
+++ app/decorators/wishlist_decorator.rb
@@ -2,14 +2,6 @@ class WishlistDecorator < Draper::Decorator
   include Draper::LazyHelpers
   delegate_all
 
-  def show_edit_link(item)
-    render 'edit_link', item: item if params[:user_id].present?
-  end
-
-  def show_delete_link(item)
-    render 'delete_link', item: item if params[:user_id].present?
-  end
-
   def show_add_item_card
     render 'add_item' if params[:user_id].present?
   end
