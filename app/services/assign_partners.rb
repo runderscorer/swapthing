babfdbee7ef80861ea256bd5d4955ee950b401d5
Rick Rieta <rickrieta@gmail.com>
2024-12-07 14:04:39 -0800

    Simplify partnering logic

--- app/services/assign_partners.rb
+++ app/services/assign_partners.rb
@@ -2,29 +2,28 @@ class AssignPartners
   def self.call(event_id, attempts = 0)
     return false if attempts > 5
 
-    event = Event.find event_id
-    users = event.users
+    event = Event.find(event_id)
+    givers = event.users
 
-    exclusions = users.each_with_object({}) do |user, hash|
-      hash[user.id] = user.exclusion.excluded_user_id if user.has_exclusion?
-    end
+    getters = Set.new
 
-    users = users.shuffle
+    givers.each do |giver|
+      getter = givers.sample
 
-    assigned = Set.new
-    
-    users.each do |user|
-      partner = users.find do |u|
-        u.id != user.id && u.id != exclusions[user.id] && !assigned.include?(u.id)
+      while getter == giver || getters.include?(getter) || giver.exclusion&.excluded_user_id == getter.id
+        getter = givers.sample
       end
 
-      if partner
-        assigned.add(partner.id)
-        Partnership.create!(event_id: event_id, giver_id: user.id, getter_id: partner.id)
-      else
-        event.partnerships.destroy_all
-        AssignPartners.call(event_id, attempts + 1)
-      end
+      getters.add(getter)
+
+      Partnership.create!(giver_id: giver.id, getter_id: getter.id, event_id: event_id)
     end
+
+    if event.partnerships.count != givers.count
+      event.partnerships.destroy_all
+      AssignPartners.call(event_id, attempts + 1)
+    end
+
+    true
   end
 end
